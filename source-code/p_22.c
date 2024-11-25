8455691b78d24cb5ee7623a1fe441b24d2cf527a
Dhruv Jain <75975797+jnDhruv@users.noreply.github.com>
2024-11-25 16:19:22 +0530

    update: p_22.c

--- source-code/p_22.c
+++ source-code/p_22.c
@@ -1,46 +1,30 @@
 // 22. Check whether given number is Armstrong.
 
 #include <stdio.h>
-
-int power(int base, int exp)
-{
-    int result = 1;
-    for (int i = 1; i <= exp; i++)
-    {
-        result = result * base;
-    }
-
-    return result;
-}
+#include <math.h>
 
 int main()
 {
     int orignum, tempnum;
-
     printf("Enter The Number You Want To Check If It Is Armstrong Or Not\n");
     scanf("%d", &orignum);
 
     tempnum = orignum;
 
     int digits = 0;
-
     while (tempnum != 0)
     {
-
         tempnum = tempnum / 10;
-
         digits = digits + 1;
     }
 
     tempnum = orignum;
 
     int backdig, result = 0;
-
     while (tempnum != 0)
     {
         backdig = tempnum % 10;
-        result = result + power(backdig, digits);
-
+        result = result + pow(backdig, digits);
         tempnum = tempnum / 10;
     }
 
