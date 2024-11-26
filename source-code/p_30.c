96c0e18920cdfff8b6891c19cdce35ee8f1d8552
prathamrathore115 <github115ptr@gmail.com>
2024-11-26 18:03:03 +0530

    added: 28,30,32

---
+++ source-code/p_30.c
@@ -0,0 +1,27 @@
+// 30. Inverted right angled pyramid
+
+//    *
+//   **
+//  ***
+// ****
+
+#include <stdio.h>
+
+int main(){
+
+    int rows;
+    printf("Enter Numbers Of Rows : ");
+    scanf("%d" , &rows);
+
+    for (int i = 0; i < rows ; i++){
+        for (int spaces = 0; spaces < rows - i - 1; spaces++){
+            printf(" ");
+        }
+
+        for (int stars = 0 ; stars <= i ; stars++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
